7431cd9963a5fe23762a0c5cfc21435f5c33857c
Alexandre <clemencot.alexandre@gmail.com>
2023-01-12 14:37:44 +0100

    Ajout de l'affichage des notes sur la MainActivity.

--- app/src/main/java/fr/alex/notepadv2/MainActivity.kt
+++ app/src/main/java/fr/alex/notepadv2/MainActivity.kt
@@ -2,10 +2,47 @@ package fr.alex.notepadv2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    lateinit var notes: MutableList<Note>
+    lateinit var adapter: NoteAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        notes = mutableListOf<Note>()
+        notes.add(Note("Alexandre", "Aller chercher les enfants"))
+        notes.add(Note("Alexandre", "Aller chercher les enfants"))
+        notes.add(Note("Alexandre", "Aller chercher les enfants"))
+        notes.add(Note("Alexandre", "Aller chercher les enfants"))
+        notes.add(Note("Alexandre", "Aller chercher les enfants"))
+        notes.add(Note("Alexandre", "Aller chercher les enfants"))
+        notes.add(Note("Alexandre", "Aller chercher les enfants"))
+        notes.add(Note("Alexandre", "Aller chercher les enfants"))
+        notes.add(Note("Alexandre", "Aller chercher les enfants"))
+        notes.add(Note("Alexandre", "Aller chercher les enfants"))
+        notes.add(Note("Alexandre", "Aller chercher les enfants"))
+        notes.add(Note("Alexandre", "Aller chercher les enfants"))
+        notes.add(Note("Alexandre", "Aller chercher les enfants"))
+        notes.add(Note("Alexandre", "Aller chercher les enfants"))
+
+        adapter = NoteAdapter(notes, this)
+
+        val recyclerView = findViewById(R.id.notes_recycler_view) as RecyclerView
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = adapter
+
+    }
+
+    override fun onClick(view: View) {
+        if (view.tag != null) {
+            Log.i("MainActivity", "Click sur une note de la liste !")
+        }
     }
 }
