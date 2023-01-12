d1e7d6cbea2a85416595787c422411cc8b522156
Alexandre <clemencot.alexandre@gmail.com>
2023-01-12 17:09:38 +0100

    Ajout creation de note

--- app/src/main/java/fr/alex/notepadv2/MainActivity.kt
+++ app/src/main/java/fr/alex/notepadv2/MainActivity.kt
@@ -1,5 +1,6 @@
 package fr.alex.notepadv2
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -20,6 +21,8 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         // Afficher la toolbar attention à bien utiliser androidx dans l'import
         val toolbar = findViewById(R.id.toolbar) as Toolbar
         setSupportActionBar(toolbar)
+
+        findViewById(R.id.create_note_fab).setOnClickListener(this)
 
         notes = mutableListOf<Note>()
         notes.add(Note("Alexandre", "Aller chercher les enfants"))
@@ -46,14 +49,48 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode != Activity.RESULT_OK || data == null) {
+            return
+        }
+
+        when (requestCode) {
+            NoteDetailActivity.REQUEST_EDIT_NOTE -> processEditNoteResult(data)
+        }
+    }
+
+    private fun processEditNoteResult(data: Intent) {
+        val noteIndex = data.getIntExtra(NoteDetailActivity.EXTRA_NOTE_INDEX, -1)
+        val  note = data.getParcelableExtra<Note>(NoteDetailActivity.EXTRA_NOTE)
+        saveNote(note, noteIndex)
+    }
+
+    private fun saveNote(note: Note?, noteIndex: Int) {
+        if (noteIndex < 0) {
+            if (note != null) {
+                notes.add(0, note)
+            }
+        } else {
+            if (note != null) {
+                notes[noteIndex] = note
+            }
+
+        }
+        adapter.notifyDataSetChanged()
     }
 
     // Affichage de la note
     override fun onClick(view: View) {
         if (view.tag != null) {
             showNoteDetail(view.tag as Int)
+        } else {
+            when(view.id) {
+                R.id.create_note_fab -> createNote()
+            }
         }
+    }
+
+    fun createNote() {
+        showNoteDetail(-1)
     }
 
     fun showNoteDetail(noteIndex: Int) {
